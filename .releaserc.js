7c3694c9a8c7502df38eafc85a98aaaef169392e
d0whc3r <d0whc3r@gmail.com>
2021-07-11 23:58:54 +0200

    fix: update relase

--- .releaserc.js
+++ .releaserc.js
@@ -16,13 +16,6 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     // '@semantic-release/npm',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['dist/**/*', 'docs', 'package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }
-    ],
     '@semantic-release/github',
     ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md', changelogTitle: '# Changelog' }],
     ['@semantic-release/git', { assets: ['CHANGELOG.md', 'package.json'] }]
